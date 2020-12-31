d6f6d5c91b1e7c1adb497e63023588f4ba9fa068
Kristof Juhasz <kristof.juhasz.asap@gmail.com>
2020-12-31 18:32:05 +0100

    Adding login component skeleton

--- ms2/angular/src/app/app-routing.module.ts
+++ ms2/angular/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', component: LoginComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
